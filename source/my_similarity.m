b9a9938bb39f2a808e44502ed805cee26abe5f88
Mike Winters <mikewinters@lawn-143-215-53-253.lawn.gatech.edu>
2015-11-17 17:50:11 -0500

    Tried using the SM_Toolbox. Takes too long.

--- source/my_similarity.m
+++ source/my_similarity.m
@@ -2,7 +2,7 @@
 % SM_Toolbox: demoSMToolbox.m
 % Modified for our purposes: RMW 11/12/2015
 
-function my_similarity(filename1, filename2)
+function S_final = my_similarity(filename1, movt)
 
 % Filename1 is a filename in the present directory mp3 format
 % Filename2 is the template filenmane in potentially another directory
@@ -13,17 +13,23 @@ function my_similarity(filename1, filename2)
 
 % Now we assume filename is a two-channel mp3 (which it is for our
 % purposes)
-f_audio1 = down_mix(mp3read(filename1));
-f_audio2 = down_mix(mp3read(filename2));
+%f_audio1 = down_mix(mp3read(filename1));
+%f_audio2 = down_mix(mp3read(filename2));
 
 % The remainder of the code is a direct copy from demoSMToolbox.m
 paramPitch.winLenSTMSP = 4410;
-[f_pitch1] = audio_to_pitch_via_FB(f_audio1,paramPitch);
-[f_pitch2] = audio_to_pitch_via_FB(f_audio2,paramPitch);
+%[f_pitch1] = audio_to_pitch_via_FB(f_audio1,paramPitch);
+%[f_pitch2] = audio_to_pitch_via_FB(f_audio2,paramPitch);
 paramCENS.winLenSmooth = 11;
 paramCENS.downsampSmooth = 5;
-[f_CENS1] = pitch_to_CENS(f_pitch1,paramCENS);
-[f_CENS2] = pitch_to_CENS(f_pitch2,paramCENS);
+%[f_CENS1] = pitch_to_CENS(f_pitch1,paramCENS);
+%[f_CENS2] = pitch_to_CENS(f_pitch2,paramCENS);
+
+f_CENS1 = open([filename1 '.mat']);
+f_CENS1 = f_CENS1.f_CENS1;
+
+f_CENS2 = open([movt '.mat']);
+f_CENS2 = f_CENS2.f_CENS2;
 
 % This block takes the longest to compute
 paramVis.colormapPreset = 2;
@@ -48,14 +54,14 @@ paramThres.applyScale = 1;
 paramThres.penalty = -2;
 [S_final] = threshSM(S,paramThres);  
 
-paramVis.imagerange = [-2,1];
-paramVis.colormapPreset = 3;
-paramVis.print=1;
-paramVis.figureName='SM_final';
-handleFigure = visualizeSM(S_final,paramVis);
+% paramVis.imagerange = [-2,1];
+% paramVis.colormapPreset = 3;
+% paramVis.print=1;
+% paramVis.figureName='SM_final';
+% handleFigure = visualizeSM(S_final,paramVis);
 
-parameterMPP.fs = 22050;
-parameterMPP.featureRate = 2;
-makePlotPlayable(f_audio1, handleFigure, parameterMPP);
+% parameterMPP.fs = 22050;
+% parameterMPP.featureRate = 2;
+% makePlotPlayable(f_audio1, handleFigure, parameterMPP);
 
 
